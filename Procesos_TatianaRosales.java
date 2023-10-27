d8e01fa229932466357d59d0ebcb561b93c0a7ae
Tatiana <madkodevian@gmail.com>
2023-10-27 13:08:22 +0100

    commit1

---
+++ Procesos_TatianaRosales.java
@@ -0,0 +1,86 @@
+
+package procesos_tatianarosales;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+public class Procesos_TatianaRosales {
+
+    public static void main(String[] args) {
+
+        //APARTADO1a
+        //procesoPrueba1a("cmd", "/c", "tasklist");
+        //APARTADO 1b
+        //procesoPrueba1b("cmd", "/c", "ping google.es");
+        //APARTADO2
+        //procesoPrueba2("cmd", "/c", "ping google.es");
+        //APARTADO3
+        //procesoPrueba3("cmd", "/c", "ping google.es");
+        //APARTADO4
+        //procesoPrueba4("cmd", "/c", "dir");
+        //APARTADO5
+        //procesoPrueba5("cmd", "/c", "type fichero.txt");
+        //procesoPrueba5("cmd", "/c", "find dias fichero.txt");
+        //procesoPrueba5("cmd", "/find", "fichero.txt");
+    }
+
+    ////////////////////////////////////////////////////////////////
+    //EJECUTA COMANDO
+    private static void procesoPrueba1a(String command, String arg1, String arg2) {
+        //Prueba procesos
+        /**
+         * PARTE 1.a)Se le pasa por parámetro el comando y los argumentos y el
+         * metodo crea un proceso que ejecuta dicho comando con sus argumentos
+         */
+
+        ArrayList<String> listaElem = new ArrayList<>();
+        listaElem.add(command);
+        listaElem.add(arg1);
+        listaElem.add(arg2);
+
+        // Crear el process builder
+        ProcessBuilder pba = new ProcessBuilder(listaElem);
+        pba.inheritIO();
+        System.out.println(listaElem);
+
+        try {
+            Process p = pba.start();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    //EJECUTA COMANDO
+    private static void procesoPrueba1b(String command, String arg1, String arg2) {
+        //Prueba procesos
+        /**
+         * PARTE 1.b)Modifica el método anterior para que en este caso el
+         * proceso padre espera a que finalice el proceso hijo. Si la ejecución
+         * del comando no se realiza correctamente muestra un mensaje de error.
+         */
+
+        ArrayList<String> listaElem = new ArrayList<>();
+        listaElem.add(command);
+        listaElem.add(arg1);
+        listaElem.add(arg2);
+
+        // Crear el process builder
+        ProcessBuilder pbb = new ProcessBuilder(listaElem);
+        pbb.inheritIO();
+        System.out.println(listaElem);
+
+        try {
+            Process p = pbb.start();
+            p.waitFor();
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Error al realizar el proceso.");
+        }
+    }
+
+    
+}
